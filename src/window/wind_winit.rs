2c0dd96527f0d7fee2405189d476e344b7ccac8c
Luis Fernandes <lmmfs.1998@gmail.com>
2025-04-30 23:05:19 +0100

    feat: still try winit + glutin, still broken

--- src/window/wind_winit.rs
+++ src/window/wind_winit.rs
@@ -1,18 +1,21 @@
 #![allow(unused)]
 #[allow(dead_code)]
-//glutin imports
-use glutin::{
-    config::{Config, ConfigTemplateBuilder, GetGlConfig},
-    context::{
-        ContextApi, ContextAttributesBuilder, NotCurrentContext, PossiblyCurrentContext, Version,
-    },
-    display::GetGlDisplay,
-    prelude::*,
-    surface::{Surface, SwapInterval, WindowSurface},
-};
-
 use raw_window_handle::HasWindowHandle;
-use winit::event_loop::EventLoop;
+use winit::application::ApplicationHandler;
+use winit::event::{KeyEvent, WindowEvent};
+use winit::event_loop::ActiveEventLoop;
+use winit::keyboard::{Key, NamedKey};
+use winit::window::{Window, WindowAttributes};
+
+use glutin::config::{Config, ConfigTemplateBuilder, GetGlConfig};
+use glutin::context::{
+    ContextApi, ContextAttributesBuilder, NotCurrentContext, PossiblyCurrentContext, Version,
+};
+use glutin::display::GetGlDisplay;
+use glutin::prelude::*;
+use glutin::surface::{Surface, SwapInterval, WindowSurface};
+
+use glutin_winit::{DisplayBuilder, GlWindow};
 
 // ubi imports
 use crate::event::application_event::*;
@@ -20,14 +23,10 @@ use crate::ubiinfo;
 use crate::window::window_trait::{UBIWindow, WindowData};
 
 pub struct WinitWindow {
-    /*
-    window: winit::window::Window,
-    surface: glutin::surface::Surface<glutin::surface::WindowSurface>,
+    pub window: Window,
     context: glutin::context::PossiblyCurrentContext,
-    gl_display: glutin::display::Display,
-    event_loop: winit::event_loop::EventLoop<()>, // will use run_return()
-    event_buffer: Vec<winit::event::Event<()>>,
-    */
+    surface: glutin::surface::Surface<WindowSurface>,
+    event_loop: EventLoop<()>,
 }
 
 impl UBIWindow for WinitWindow {
@@ -35,13 +34,63 @@ impl UBIWindow for WinitWindow {
     where
         Self: Sized,
     {
-        let event_loop = EventLoop::new().map_err(|e| e.to_string())?;
+        let event_loop = EventLoop::new().unwrap();
+
+        // Create window
+        let window_builder = WindowBuilder::new()
+            .with_title("OpenGL Window")
+            .with_inner_size(LogicalSize::new(800, 600));
+        let template = ConfigTemplateBuilder::new().with_depth_size(24);
+
+        // Get display
+        let display_builder = glutin_winit::DisplayBuilder::new().with_window_builder(Some(window_builder));
+        let (window, config) = display_builder
+            .build(&event_loop, template, |configs| configs.next().unwrap())
+            .unwrap();
+
+        let window = window.unwrap();
+
+        let window_handle = window.window_handle();
+        let display = config.display();
+
+        let context_attributes = ContextAttributesBuilder::new()
+            .with_context_api(ContextApi::OpenGl(Some(glutin::context::Version::new(3, 3))))
+            .build(Some(window_handle));
+
+        let not_current_context: NotCurrentContext = unsafe {
+            display
+                .create_context(&config, &context_attributes)
+                .expect("Failed to create OpenGL context")
+        };
+
+        let attrs = SurfaceAttributesBuilder::<WindowSurface>::new().build(
+            window.window_handle(),
+            window.inner_size().width,
+            window.inner_size().height,
+        );
+
+        let surface = unsafe {
+            display
+                .create_window_surface(&config, &attrs)
+                .expect("Failed to create surface")
+        };
+
+        let context = not_current_context
+            .make_current(&surface)
+            .expect("Failed to make context current");
+
         Ok(Self {
+            window,
+            context,
+            surface,
+            event_loop,
         })
     }
 
     fn poll_events(&mut self) -> Vec<Box<dyn crate::event::event::Event>> {
-        todo!()
+        let mut out_events = Vec::new();
+
+        out_events 
     }
 
     fn get_size(&self) -> (u32, u32) {
@@ -49,14 +98,21 @@ impl UBIWindow for WinitWindow {
     }
 
     fn swap_buffers(&self) {
-        todo!()
+        self.surface.swap_buffers(&self.context).unwrap();
     }
 
     fn clear(&self) {
-        todo!()
+        unsafe {
+            gl::ClearColor(0.1, 0.1, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
     }
 
     fn resize(&self, width: i32, height: i32) {
-        todo!()
+        self.surface.resize(
+            &self.context,
+            width.try_into().unwrap(),
+            height.try_into().unwrap(),
+        );
     }
 }
